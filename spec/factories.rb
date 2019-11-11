12985bafb7af95a69f742cafd884f39ce23af2c9
Taynara Jacon <tjacon@partnerfusion.com>
2019-11-11 12:54:17 -0300

    Added new context

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,9 +11,53 @@ FactoryBot.define do
     end
   end
 
+  factory :registered_user, class: UserModel do
+    full_name { "Anakin Skywalker" }
+    email { "anakin@jedi.com" }
+    password { "jedi123" }
+
+    after(:build) do |user|
+      Database.new.delete_user(user.email)
+      ApiUser.save(user.to_hash)
+    end
+  end
+
   factory :user_wrong_email, class: UserModel do
     full_name { "Test Skywalker" }
     email { "test#jedi.com" }
     password { "jedi123" }
+  end
+
+  factory :empty_name_user, class: UserModel do
+    full_name { "" }
+    email { "test@jedi.com" }
+    password { "jedi123" }
+  end
+
+  factory :empty_email_user, class: UserModel do
+    full_name { "Test Skywalker" }
+    email { "" }
+    password { "jedi123" }
+  end
+
+  factory :empty_password_user, class: UserModel do
+    full_name { "Test Skywalker" }
+    email { "test#jedi.com" }
+    password { "" }
+  end
+
+  factory :null_name_user, class: UserModel do
+    email { "test@jedi.com" }
+    password { "jedi123" }
+  end
+
+  factory :null_email_user, class: UserModel do
+    full_name { "Test Skywalker" }
+    password { "jedi123" }
+  end
+
+  factory :null_password_user, class: UserModel do
+    full_name { "Test Skywalker" }
+    email { "test#jedi.com" }
   end
 end
